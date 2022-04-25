617eb907e232b4f48532f9d640f0020f74e43484
KolsiNetta <netta.kolsi@windowslive.com>
2022-04-25 13:48:16 +0300

    osa2 puhelinluettelo step6

--- osa2/kurssitiedot/src/components/Course.js
+++ osa2/kurssitiedot/src/components/Course.js
@@ -8,41 +8,40 @@ const Course = ({ course }) => {
         <Total parts={course.parts} />
       </div>
     )
-    
-  }
+}
 
-  const Header = (props) => {
+const Header = (props) => {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+    </div>
+  )
+}
+
+const Content = (props) => {
     return (
-      <div>
-        <h2>{props.name}</h2>
+        <div> 
+        {props.parts.map(part =>
+        <Part key={part.id} part={part} />)}
       </div>
     )
-  }
-  
-  const Content = (props) => {
-      return (
-          <div> 
-          {props.parts.map(part =>
-          <Part key={part.id} part={part} />)}
-        </div>
-      )
-      
-  }
-  
-  const Part = (props) => {
-        return(
-          <div>
-          <p>{props.part.name} {props.part.exercises} </p>
-          </div>
-      
-      )
-  }
+    
+}
 
-  const Total = ({parts}) => {
-    return (
-      <h4>total of {parts.reduce((sum,part) => sum+part.exercises,0)} exercises</h4>
+const Part = (props) => {
+      return(
+        <div>
+        <p>{props.part.name} {props.part.exercises} </p>
+        </div>
+    
     )
-  }
+}
+
+const Total = ({parts}) => {
+  return (
+    <h4>total of {parts.reduce((sum,part) => sum+part.exercises,0)} exercises</h4>
+  )
+}
   
 
 
--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,17 +1,26 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Person from './components/Person'
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',number: '0506958473' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [showAll, setShowAll] = useState(true)
   const [ filterText, setFilter] = useState('')
+
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }
+  
+  useEffect(hook, [])
 
   const addPerson = (event) => {
     event.preventDefault()
